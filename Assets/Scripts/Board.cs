0753713bb89bce161a138d126722d95d465a725b
Orlando Almario <orlax22@gmail.com>
2022-11-15 18:05:57 -0500

    Ajustando Posición de la cámara

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -10,10 +10,28 @@ public class Board : MonoBehaviour
     public int height;
     public GameObject tileObject;
 
+    public float cameraSizeOffset;
+    public float cameraVerticalOffset;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupBoard();
+        PositionCamera();
+    }
+
+    private void PositionCamera()
+    {
+        float newPosX = (float)width / 2f;
+        float newPosY = (float)height / 2f;
+
+        Camera.main.transform.position = new Vector3(newPosX - 0.5f, newPosY - 0.5f + cameraVerticalOffset, -10f);
+
+        float horizontal = width+1;
+        float vertical = (height / 2) + 1;
+
+        Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffset : vertical + cameraSizeOffset;
+
     }
 
     private void SetupBoard()
