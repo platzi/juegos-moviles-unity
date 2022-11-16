6b0ae592fdcbfe0a52c6c6123e4e2b3d9eda577f
Orlando Almario <orlax22@gmail.com>
2022-11-16 10:03:57 -0500

    Instanciando las Piezas

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -13,11 +13,28 @@ public class Board : MonoBehaviour
     public float cameraSizeOffset;
     public float cameraVerticalOffset;
 
+    public GameObject[] availablePieces;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupBoard();
         PositionCamera();
+        SetupPieces();
+    }
+
+    private void SetupPieces()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var selectedPiece = availablePieces[UnityEngine.Random.Range(0, availablePieces.Length)];
+                var o = Instantiate(selectedPiece, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
+                o.GetComponent<Piece>()?.Setup(x, y, this);
+            }
+        }
     }
 
     private void PositionCamera()
